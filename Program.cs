dcce860726e481102823a27d83e954967bfebb6e
P. Shiva <shivakumar1b2d@gmail.com>
2025-05-26 05:20:46 +0530

    Initial commit: Bank account app with custom exceptions

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class InsufficientFundsException : Exception
+{
+    public InsufficientFundsException(string message, Exception? Ex = null) : base(message, Ex){}
+}
+public class BankAccount
+{
+    public string? AccountNumber { get; private set; }
+    public decimal Balance { get; private set; }
+
+    public BankAccount(string Acc_Num, decimal balance)
+    {
+        AccountNumber = Acc_Num;
+        Balance = balance;
+    }
+
+    public void Deposit(decimal amount)
+    {
+      if (amount <= 0)
+        {
+            throw new InsufficientFundsException($"Cannot deposit {amount}. Deposit amount must be greater than 0.");
+        }
+        Balance += amount;
+        Console.WriteLine($"✅ Deposit successful! Total Balance: ₹{Balance}");
+    }
+    public void Withdraw(decimal amount)
+    {
+        if (amount > Balance)
+        {
+            throw new InsufficientFundsException($"❌ Withdrawal failed! ₹{amount} exceeds available balance of ₹{Balance}.");
+        }
+        Balance -= amount;
+        Console.WriteLine($"✅ Withdrawal successful! Current Balance: ₹{Balance}");
+    }
+}
+
+
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            BankAccount bankaccount = new BankAccount("123456123889", 1000);
+            bankaccount.Deposit(500);
+            Console.WriteLine("Deposit Completed");
+            bankaccount.Withdraw(700);
+            Console.WriteLine("withdraw Completed");
+
+            bankaccount.Withdraw(2000);
+        }
+        catch (InsufficientFundsException ex)
+        {
+            Console.WriteLine("Error: " + ex.Message);
+        }
+        finally
+        {
+            Console.WriteLine("Transaction Ended");
+        }
+    }
+}
